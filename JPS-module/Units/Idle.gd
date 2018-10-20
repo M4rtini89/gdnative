dd2598eb7bb3e95b84b383cd23d8a95bb198533a
MartinVN <m4rtini89@gmail.com>
2018-10-21 00:31:11 +0200

    small update to idle state, so it doesent fly to infinity when bumped.

--- JPS-module/Units/Idle.gd
+++ JPS-module/Units/Idle.gd
@@ -1,5 +1,14 @@
 extends "res://Utils/State/State.gd"
 
+var idle_pos = Vector2()
+
 func enter(params=null):
+	# TODO: Make it move out of the way for allied units. 
+	# Possibly seek back to idle pos afterwards.
+	idle_pos = Vector2()
 	print("entered idle state")
+	owner.linear_damp = 5
+
+func exit():
+	owner.linear_damp = -1
 
